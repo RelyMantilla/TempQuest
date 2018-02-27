ba8071394fc2baf649203981d29d6b4c14658309
fenix <rely.m.v@gmail.com>
2018-02-27 12:49:35 -0500

    correccion animaciones

--- personaje.gd
+++ personaje.gd
@@ -5,6 +5,7 @@ var gravedad = 10
 var velocidad_lateral=200
 var velocidad_salto = 200
 var grupo = 0
+var volando = false
 export(int, FLAGS, "player1", "player2") var jugador = 0
 func _ready():
 	velocidad.x = 0
@@ -16,18 +17,27 @@ func _physics_process(delta):
 	if (Input.is_action_pressed(str("player",str(jugador) ,"_der"))):
 		velocidad.x = velocidad_lateral
 		$Sprite.flip_h = true
-		if !$Sprite.is_playing():
-			$Sprite.play(str("Walk",str(jugador)))
+#		if !$Sprite.is_playing():
+#			$Sprite.play(str("Walk",str(jugador)))
 	elif (Input.is_action_pressed(str("player",str(jugador) ,"_izq"))):
 		velocidad.x = -velocidad_lateral
 		$Sprite.flip_h = false
-		if !$Sprite.is_playing():
-			$Sprite.play(str("Walk",str(jugador)))
 	else:
-		velocidad.x = 0 
+		velocidad.x = 0
 	if (Input.is_action_pressed(str("player",str(jugador) ,"_up"))):
 		velocidad.y = -velocidad_salto
+		volando=true
+
+	#animaciones
+	if volando: #esta volando 
 		$Sprite.play(str("Volar",str(jugador)))
-
-
-	velocidad = move_and_slide(velocidad) 
+	elif velocidad.x != 0: #esta moviendo izquierda
+		$Sprite.play(str("Walk",str(jugador)))
+	else: #esta detenido
+		$Sprite.stop()
+	
+	#Se restablece la variable al tocar el piso para cambiar la animacion
+	if is_on_floor():
+		volando = false
+	
+	velocidad = move_and_slide(velocidad, Vector2(0,-1)) 
